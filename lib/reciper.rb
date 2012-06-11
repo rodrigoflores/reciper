740a404499363861381ed4bb3dbf14c0d72e841a
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-06-11 15:12:02 -0300

    Initialize function

--- lib/reciper.rb
+++ lib/reciper.rb
@@ -1,3 +1,20 @@
+require "active_support"
+require "active_support/core_ext/string"
+
 class Reciper
   require "reciper/helpers"
+
+  attr_reader :name, :source_path, :ruby_app_path
+
+  def initialize(name, source_path, ruby_app_template_path)
+    @name = name
+    @source_path = source_path
+    @ruby_app_path = File.join(".", "tmp", name.parameterize("_"))
+
+    if File.directory?(@ruby_app_path)
+      FileUtils.rm_rf(@ruby_app_path)
+    end
+
+    FileUtils.cp_r(ruby_app_template_path, @ruby_app_path)
+  end
 end
---
+++ spec/reciper/reciper_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+describe Reciper do
+  describe "#initialize" do
+    it "assigns the paths" do
+      File.stub!(:directory?).and_return(false)
+      FileUtils.stub!(:cp_r)
+
+      reciper = Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+
+      reciper.ruby_app_path.should == "./tmp/awesome_recipe"
+      reciper.source_path.should == "~/Code/source_path"
+    end
+
+    it "removes the temp directory if it exists and copies a new version" do
+      File.should_receive(:directory?).
+        with("./tmp/awesome_recipe").and_return(true)
+
+      FileUtils.should_receive(:rm_rf).with("./tmp/awesome_recipe")
+
+      FileUtils.stub!(:cp_r)
+
+      Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+    end
+
+    it "creates the temp directory" do
+      File.stub!(:directory?).and_return(false)
+
+      FileUtils.should_receive(:cp_r).with("~/Code/rails_app", "./tmp/awesome_recipe")
+
+      Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+    end
+  end
+
+  describe "#execute" do
+    it "run an instance eval"
+  end
+end
