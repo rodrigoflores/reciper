6533d04f886cf4de0833a79989f8bbf95f7fdeb1
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-06-11 15:50:33 -0300

    Execute and source_path to recipe_path

--- lib/reciper.rb
+++ lib/reciper.rb
@@ -4,11 +4,11 @@ require "active_support/core_ext/string"
 class Reciper
   require "reciper/helpers"
 
-  attr_reader :name, :source_path, :ruby_app_path
+  attr_reader :name, :recipe_path, :ruby_app_path
 
-  def initialize(name, source_path, ruby_app_template_path)
+  def initialize(name, recipe_path, ruby_app_template_path)
     @name = name
-    @source_path = source_path
+    @recipe_path = recipe_path
     @ruby_app_path = File.join(".", "tmp", name.parameterize("_"))
 
     if File.directory?(@ruby_app_path)
@@ -16,5 +16,11 @@ class Reciper
     end
 
     FileUtils.cp_r(ruby_app_template_path, @ruby_app_path)
+  end
+
+  def execute(&block)
+    if block_given?
+      instance_eval(&block)
+    end
   end
 end
--- spec/reciper/reciper_spec.rb
+++ spec/reciper/reciper_spec.rb
@@ -8,10 +8,10 @@ describe Reciper do
     end
 
     it "assigns the paths" do
-      reciper = Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+      reciper = described_class.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
 
       reciper.ruby_app_path.should == "./tmp/awesome_recipe"
-      reciper.source_path.should == "~/Code/source_path"
+      reciper.recipe_path.should == "~/Code/source_path"
     end
 
     it "removes the temp directory if it exists and copies a new version" do
@@ -20,19 +20,27 @@ describe Reciper do
 
       FileUtils.should_receive(:rm_rf).with("./tmp/awesome_recipe")
 
-      Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+      described_class.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
     end
 
     it "creates the temp directory" do
       FileUtils.should_receive(:cp_r).with("~/Code/rails_app", "./tmp/awesome_recipe")
 
-      Reciper.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+      described_class.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
     end
   end
 
   describe "#execute" do
-    it "run an instance eval" do
-      pending
+    it "run an instance eval with all paths" do
+      File.stub!(:directory?).and_return(false)
+      FileUtils.stub!(:cp_r)
+
+      reciper = described_class.new("Awesome recipe", "~/Code/source_path", "~/Code/rails_app")
+
+      reciper.execute do
+        @ruby_app_path.should == "./tmp/awesome_recipe"
+        @recipe_path.should == "~/Code/source_path"
+      end
     end
   end
 end
